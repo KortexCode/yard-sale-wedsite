0ce4e78f1f12c3959337025a0230b63926d553b3
kortexcode <darkmaeno323@gmail.com>
2023-03-08 11:20:28 -0500

    Se agregan rutas al directorio routes, se crea ErrorPage

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -3,14 +3,15 @@ import { createHashRouter, createRoutesFromElements, Route } from "react-router-
 import { LoginPage } from "../pages/LoginPage";
 import { Layout } from "../layout/LayoutPublic";
 import { HomePage } from "../pages/HomePage";
-import { LogIn } from "../components/LogIn";
-import { SignUp } from "../components/SignUp";
-import { CreateNewPassword } from "../components/CreateNewPassword";
-import { EmailSend } from "../components/EmailSend";
+import { LogIn } from "../routes/LogIn";
+import { SignUp } from "../routes/SignUp";
+import { CreateNewPassword } from "../routes/CreateNewPassword";
+import { EmailSend } from "../routes/EmailSend";
+import { ErroPage } from "../routes/ErrorPage";
 
 const router = createHashRouter(
     createRoutesFromElements(
-        <Route path="/" element={<Layout/>} errorElement>
+        <Route path="/" element={<Layout/>} errorElement={<ErroPage/>} >
             <Route index={true} path="/" element={<HomePage/>}/>
             <Route path="/login" element={<LoginPage/>}>
                 <Route index={true} path="/login" element={<LogIn/>}/>
---
+++ src/routes/ErrorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ErroPage(){
+    return (
+        <h2>
+            ERROR PAGE Chikorita!!
+        </h2>
+    )
+}
+
+export {ErroPage}
