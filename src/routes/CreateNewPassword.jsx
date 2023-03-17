c18c6039dbeed6980c14fc43838d9e1e1fbaa3ed
kortexcode <darkmaeno323@gmail.com>
2023-03-17 09:58:34 -0500

    Se hacen cambios a la lógica de cambio de contraseña en createNewPassword.jsx; se usa FormData y useRef

--- src/routes/CreateNewPassword.jsx
+++ src/routes/CreateNewPassword.jsx
@@ -1,51 +1,50 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useNavigate, useOutletContext } from 'react-router-dom';
 import logoYardSale from '@logos/logo_yard_sale.svg';
 import "@styles/CreateNewPassword.scss";
 
 function CreateNewPassword(){
   const {changePassword, userData} = useOutletContext();
-  const [emailInput, setEmailInput] = React.useState("");
-  const [passwordInput, setPasswordInput] = React.useState("");
   const [empty, setEmpty] = React.useState(false); //Para valida campos vacíos
   const [validEmail, setValidEmail] = React.useState(false); //valida email errado
   const navigate = useNavigate();
+  const form = useRef(null);//Se ferencia el formulario
 
   //Evento de click
   const handleConfirm = ()=>{//Genera cambio de contraseña
+    //Creamos un objeto FormData para extrear los datos del atributo name de los inputs
+    let formData = new FormData(form.current);
+    console.log("oye", formData.get('email'));
+    const {email, newPassword} = Object.fromEntries(formData);//Se obtienen las entradas
+
     //Se buscan campos vacios
-    const emptyState = emailInput.length == 0 ? true : passwordInput.length == 0 ? true 
-    : false;
-    if(emptyState){
-        setEmpty(true);
-        return;
+    const emptyState = email.length == 0 ? true 
+    : newPassword.length == 0 ? true : false;
+    if(emptyState){//Se hay campos vacios se muestra mensaje de error
+      setEmpty(true);
+      return;
     }
-    //se extrae lista de usuarios de la base de datos
+    //De no haber campos vacios se extrae lista de usuarios de la base de datos
     let userListInDataBase = [...userData];
     //Se busca si existe el email insertado por el usuario
     const userEmail = userListInDataBase.find((user)=>{
-      return emailInput === user.email  
+      return email === user.email  
     });
-    console.log("ele mail", userEmail)
-    if(!userEmail){
+    if(!userEmail){//Si no existe el email se muestra mensaje de error
       setValidEmail(true);
       return
     }
-    else{
-      console.log("entro a cambiar")
-      changePassword(emailInput, passwordInput);
+    else{//De lo contrario se hace el cambio de constraseña
+      changePassword(email, newPassword);
       navigate("/log-in");
     }
     
   }
-
   //Eventos de cambios en los input
-  const handleInserEmail = ({target}) => {  
-    setEmailInput(target.value);
+  const handleInserEmail = () => {  
     restoreStates();
   }
-  const handleInserNewPassword = ({target}) => {  
-    setPasswordInput(target.value);
+  const handleInserNewPassword = () => {  
     restoreStates();
   }
   //Restaura mensaje de error
@@ -56,18 +55,19 @@ function CreateNewPassword(){
       setValidEmail(false);
   }
 
+
   return(
       <div className="form-container">
         <img src={logoYardSale} alt="logo" className="logo"/>  
         <h1 className="title">Create a new password</h1>
         <p className="subtitle">Enter a new password for you account</p>
-        <form className="form">
+        <form action="/" className="form" ref={form}>
           <label htmlFor="password" className="label">Email</label>
           {validEmail && <p>This email is not registered</p>}
-          <input type="text" id="password" placeholder="insert your email"
+          <input type="text" /* id="password" */ name='email' placeholder="insert your email"
            className="input input-password" onChange={handleInserEmail}/> 
           <label htmlFor="new-password" className="label">Password</label>
-          <input type="password" id="new-password" placeholder="*********" 
+          <input type="password" /* id="new-password" */ name='newPassword' placeholder="*********" 
           className="input input-password" onChange={handleInserNewPassword}/>  
           {empty && <p>There are empty fields</p>}
           <input type="button" className="primary-button login-button" value="Confirm" onClick={handleConfirm}/>
