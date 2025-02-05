fab485458429753284031ed6efad15e68caf97a4
KortexDev <cacortesl01@gmail.com>
2025-02-05 14:50:27 -0500

    se aumenta el número de productos a 40

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,106 +1,105 @@
-import React, { useEffect } from 'react'
-import { useLocationStorage } from './useLocationStorage';
+import React from "react";
+import { useLocationStorage } from "./useLocationStorage";
 
-function useAuth(){
+function useAuth() {
+  const { userData, setUserData } = useLocationStorage([], "item_V1"); //Base de datos local
 
-    const {userData, setUserData} = useLocationStorage([], "item_V1");//Base de datos local
- 
-    //ESTADOS
-    const [username, setUserName] = React.useState(null);
-    //Menus y Asides
-    const [openDesktopMenu, setOpenDesktopMenu] = React.useState(false);
-    const [showMobileMenu, setShowMobileMenu] = React.useState(false);
-    const [openProductDetail, setOpenProductDetail] = React.useState(false);
-    const [openShoppingCart, setOpenShoppingCart] = React.useState(false);
-    const [openOrderMenu, setOpenOrderMenu] = React.useState(false);
-    //Lista de productos y datos de productos
-    const [shoppingList, setShoppingList] = React.useState([]);
-    const [productId, setProductId] = React.useState(null);
-    const [orderList, setOrderList] = React.useState([]);
-    const [orderListId, setOrderListId] = React.useState(null);
-    const [loading, setLoading] = React.useState(true);
-    //Autenticar usuario
-    function authUser(user_name){
-        if(!user_name){//Cuando se haga logout
-            setUserName(null);
-            return;
-        }else{
-            let userListInDataBase = [...userData];
-            //Se busca el usuario actualmente logueado
-            const userlogued = userListInDataBase.find((user)=>{
-                return user_name === user.name
-            
-            });
-            setOrderList(userlogued?.ordersList);
-            setUserName(user_name);
-        }    
+  //ESTADOS
+  const [username, setUserName] = React.useState(null);
+  //Menus y Asides
+  const [openDesktopMenu, setOpenDesktopMenu] = React.useState(false);
+  const [showMobileMenu, setShowMobileMenu] = React.useState(false);
+  const [openProductDetail, setOpenProductDetail] = React.useState(false);
+  const [openShoppingCart, setOpenShoppingCart] = React.useState(false);
+  const [openOrderMenu, setOpenOrderMenu] = React.useState(false);
+  //Lista de productos y datos de productos
+  const [shoppingList, setShoppingList] = React.useState([]);
+  const [productId, setProductId] = React.useState(null);
+  const [orderList, setOrderList] = React.useState([]);
+  const [orderListId, setOrderListId] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
+  //Autenticar usuario
+  function authUser(user_name) {
+    if (!user_name) {
+      //Cuando se haga logout
+      setUserName(null);
+      return;
+    } else {
+      let userListInDataBase = [...userData];
+      //Se busca el usuario actualmente logueado
+      const userlogued = userListInDataBase.find((user) => {
+        return user_name === user.name;
+      });
+      setOrderList(userlogued?.ordersList);
+      setUserName(user_name);
     }
-    //Registrar usuario
-    function userSignUp(data){
-        const userList = [...userData];
-        userList.push(data);
-        setUserData(userList);
-        localStorage.setItem("item_V1", JSON.stringify(userList));    
-    }
-    function addOrdersToUserList(newOrder){
-        //se extrae lista de usuarios de la base de datos
-        let userListInDataBase = [...userData];
-        //Se busca el usuario actualmente logueado
-        userListInDataBase = userListInDataBase.map((user)=>{
-            if(username === user.name){
-                //Se agrega la nueva orden a la lista de ordenes del usuario logueado
-                user.ordersList.push(newOrder);
-                setOrderList([...user.ordersList]);
-            };
-            return user;//se retornan todos los usuarios
-        })
-        //Se actualizan los datos de usuario registrados; 
-        setUserData(userListInDataBase)
-        localStorage.setItem("item_V1", JSON.stringify(userListInDataBase));  
-    }
-    function changePassword(userEmail, newPassword){
-        //se extrae lista de usuarios de la base de datos
-        let userListInDataBase = [...userData];
-        //Se busca el usuario actualmente logueado
-        userListInDataBase = userListInDataBase.map((user)=>{
-          if(userEmail === user.email){
-            user.password = newPassword;
-          }
-          return user;
-        });
-        setUserData(userListInDataBase);
-        localStorage.setItem("item_V1", JSON.stringify(userListInDataBase)); 
-
-    }
-    return [{
-        userData,
-        username,
-        productId,
-        openDesktopMenu,
-        showMobileMenu,
-        openProductDetail,
-        openShoppingCart,
-        shoppingList,
-        orderList,
-        orderListId,
-        openOrderMenu,
-        loading,
-        authUser,
-        setProductId,
-        setOpenDesktopMenu,
-        setShowMobileMenu,
-        setOpenProductDetail,
-        setOpenShoppingCart,
-        setShoppingList,
-        setOrderList,
-        addOrdersToUserList,
-        setOrderListId,
-        setOpenOrderMenu,
-        userSignUp,
-        changePassword,
-        setLoading,
-    } ] 
-    
+  }
+  //Registrar usuario
+  function userSignUp(data) {
+    const userList = [...userData];
+    userList.push(data);
+    setUserData(userList);
+    localStorage.setItem("item_V1", JSON.stringify(userList));
+  }
+  function addOrdersToUserList(newOrder) {
+    //se extrae lista de usuarios de la base de datos
+    let userListInDataBase = [...userData];
+    //Se busca el usuario actualmente logueado
+    userListInDataBase = userListInDataBase.map((user) => {
+      if (username === user.name) {
+        //Se agrega la nueva orden a la lista de ordenes del usuario logueado
+        user.ordersList.push(newOrder);
+        setOrderList([...user.ordersList]);
+      }
+      return user; //se retornan todos los usuarios
+    });
+    //Se actualizan los datos de usuario registrados;
+    setUserData(userListInDataBase);
+    localStorage.setItem("item_V1", JSON.stringify(userListInDataBase));
+  }
+  function changePassword(userEmail, newPassword) {
+    //se extrae lista de usuarios de la base de datos
+    let userListInDataBase = [...userData];
+    //Se busca el usuario actualmente logueado
+    userListInDataBase = userListInDataBase.map((user) => {
+      if (userEmail === user.email) {
+        user.password = newPassword;
+      }
+      return user;
+    });
+    setUserData(userListInDataBase);
+    localStorage.setItem("item_V1", JSON.stringify(userListInDataBase));
+  }
+  return [
+    {
+      userData,
+      username,
+      productId,
+      openDesktopMenu,
+      showMobileMenu,
+      openProductDetail,
+      openShoppingCart,
+      shoppingList,
+      orderList,
+      orderListId,
+      openOrderMenu,
+      loading,
+      authUser,
+      setProductId,
+      setOpenDesktopMenu,
+      setShowMobileMenu,
+      setOpenProductDetail,
+      setOpenShoppingCart,
+      setShoppingList,
+      setOrderList,
+      addOrdersToUserList,
+      setOrderListId,
+      setOpenOrderMenu,
+      userSignUp,
+      changePassword,
+      setLoading,
+    },
+  ];
 }
 
-export {useAuth}
+export { useAuth };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,18 @@
 import React, { useEffect } from "react";
 import { Menu } from "@components/Menu";
 import "@styles/HomePage.scss";
-import { defer, useLoaderData, useOutletContext, useParams } from "react-router-dom";
+import {
+  defer,
+  useLoaderData,
+  useOutletContext,
+  useParams,
+} from "react-router-dom";
 import { ProductCart } from "@components/ProductCart";
 import { ProductDetail } from "@components/ProductDetail";
 import { ShoppingCart } from "@components/ShoppingCart";
 import { usePagination } from "../hooks/usePagination";
 
-function HomePage(){
-
+function HomePage() {
   const {
     userData,
     username,
@@ -31,81 +35,105 @@ function HomePage(){
     addOrdersToUserList,
     authUser,
   } = useOutletContext();
-  const [dataApi, setDataApi] = React.useState([]);//Estado donde se guardan datos de la Api
+  const [dataApi, setDataApi] = React.useState([]); //Estado donde se guardan datos de la Api
   const [category, setCategory] = React.useState(null);
-  const {id}= useParams();//Obtenemos parámetro id de las categorías
-  const {pagination, setPagination} = usePagination();//Obtenemos datos de la paginación 
-  const {limit} = {...pagination};//Elementos de la paginación
+  const { id } = useParams(); //Obtenemos parámetro id de las categorías
+  const { pagination, setPagination } = usePagination(); //Obtenemos datos de la paginación
+  const { limit } = { ...pagination }; //Elementos de la paginación
   //Este use effect genera la consulta a la Api cada vez que hayan cambios en la
   //paginación o el id de navegación.
-  useEffect(()=>{
-    if(id != category){//Al cambiar la categoría reiniciará el scroll, paginación y categoría
-      setPagination({...pagination, ...{limit: 10}});
+  useEffect(() => {
+    if (id != category) {
+      //Al cambiar la categoría reiniciará el scroll, paginación y categoría
+      setPagination({ ...pagination, ...{ limit: 40 } });
       setCategory(id);
       window.scrollTo(0, 0);
       return;
     }
-    if(limit > 50){//Si se pasa de la página 50 no hará la consulta
-      return
+    if (limit > 50) {
+      //Si se pasa de la página 50 no hará la consulta
+      return;
     }
-  
-    async function productsData(){
-      
+
+    async function productsData() {
       //El slug indicará en cual categoría de productos está buscando información
       const slug = id ? `categories/${id}/products` : "products";
-      const res = await fetch(`https://api.escuelajs.co/api/v1/${slug}?offset=0&limit=${limit}`);
+      const res = await fetch(
+        `https://api.escuelajs.co/api/v1/${slug}?offset=0&limit=${limit}`
+      );
       const loaderData = await res.json();
-      if(!loaderData.length){
+      if (!loaderData.length) {
         return;
       }
-      if(loading){
+      if (loading) {
         setLoading(false);
       }
       setCategory(id);
       setDataApi(loaderData);
     }
     productsData();
-      
-  }, [pagination, id]); 
+  }, [pagination, id]);
 
-    return(
-        <>
-            <Menu username={username} openDesktopMenu={openDesktopMenu} 
-              setOpenDesktopMenu={setOpenDesktopMenu} showMobileMenu={showMobileMenu}
-              setShowMobileMenu={setShowMobileMenu} authUser={authUser} 
-              setOpenShoppingCart={setOpenShoppingCart} shoppingList={shoppingList}
-              openShoppingCart={openShoppingCart} setOpenProductDetail={setOpenProductDetail}>
-            </Menu>
-            <ProductDetail dataApi={dataApi} openProductDetail={openProductDetail}
-             setOpenProductDetail={setOpenProductDetail} productId={productId}
-             shoppingList={shoppingList} setShoppingList={setShoppingList}/>
-            <ShoppingCart username={username} dataApi={dataApi} openShoppingCart={openShoppingCart}
-              setOpenShoppingCart={setOpenShoppingCart} setShoppingList={setShoppingList}
-              shoppingList={shoppingList} setOrderList={setOrderList} 
-              addOrdersToUserList={addOrdersToUserList} 
-              orderList={orderList} userData={userData} />
-            <section className="main-container">
-                <div className="cards-container">
-                  {loading && <div className="skeleton--loading"></div>}
-                  {loading && <div className="skeleton--loading"></div>}
-                  {loading && <div className="skeleton--loading"></div>}
-                  {loading && <div className="skeleton--loading"></div>}
-                  {loading && <div className="skeleton--loading"></div>}
-                  {loading && <div className="skeleton--loading"></div>}
-                  {loading && <div className="skeleton--loading"></div>}
+  return (
+    <>
+      <Menu
+        username={username}
+        openDesktopMenu={openDesktopMenu}
+        setOpenDesktopMenu={setOpenDesktopMenu}
+        showMobileMenu={showMobileMenu}
+        setShowMobileMenu={setShowMobileMenu}
+        authUser={authUser}
+        setOpenShoppingCart={setOpenShoppingCart}
+        shoppingList={shoppingList}
+        openShoppingCart={openShoppingCart}
+        setOpenProductDetail={setOpenProductDetail}
+      ></Menu>
+      <ProductDetail
+        dataApi={dataApi}
+        openProductDetail={openProductDetail}
+        setOpenProductDetail={setOpenProductDetail}
+        productId={productId}
+        shoppingList={shoppingList}
+        setShoppingList={setShoppingList}
+      />
+      <ShoppingCart
+        username={username}
+        dataApi={dataApi}
+        openShoppingCart={openShoppingCart}
+        setOpenShoppingCart={setOpenShoppingCart}
+        setShoppingList={setShoppingList}
+        shoppingList={shoppingList}
+        setOrderList={setOrderList}
+        addOrdersToUserList={addOrdersToUserList}
+        orderList={orderList}
+        userData={userData}
+      />
+      <section className="main-container">
+        <div className="cards-container">
+          {loading && <div className="skeleton--loading"></div>}
+          {loading && <div className="skeleton--loading"></div>}
+          {loading && <div className="skeleton--loading"></div>}
+          {loading && <div className="skeleton--loading"></div>}
+          {loading && <div className="skeleton--loading"></div>}
+          {loading && <div className="skeleton--loading"></div>}
+          {loading && <div className="skeleton--loading"></div>}
 
-                  {!loading && dataApi?.map((product)=> <ProductCart key={product.id}
-                  product={product}
-                  setOpenProductDetail={setOpenProductDetail} 
-                  openProductDetail={openProductDetail}
-                  setProductId={setProductId} setshoppingList={setShoppingList}
-                  shoppingList={shoppingList} />)}
-                          
-                </div>
-            </section>
-        </>
-    )
+          {!loading &&
+            dataApi?.map((product) => (
+              <ProductCart
+                key={product.id}
+                product={product}
+                setOpenProductDetail={setOpenProductDetail}
+                openProductDetail={openProductDetail}
+                setProductId={setProductId}
+                setshoppingList={setShoppingList}
+                shoppingList={shoppingList}
+              />
+            ))}
+        </div>
+      </section>
+    </>
+  );
 }
 
-export {HomePage}
-
+export { HomePage };
