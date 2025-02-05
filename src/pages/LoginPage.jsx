74ce3acc58a7448bdc79403144b8ca7dc404ee26
KortexDev <cacortesl01@gmail.com>
2025-02-05 14:43:21 -0500

    Cambios visuales a las cards y login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,15 +1,26 @@
 import React from "react";
-import { Outlet, useOutletContext } from "react-router-dom";
+import { Outlet, useNavigate, useOutletContext } from "react-router-dom";
+import arrowIcon from "@icons/flechita.svg";
 import "../styles/LoginPage.scss";
 
-function LoginPage(){
-    const auth = useOutletContext();
-  
-    return (
-      <div className="login">
-        <Outlet context={auth}/>
-      </div>
-    )
+function LoginPage() {
+  const auth = useOutletContext();
+  const navigate = useNavigate();
+  const handleBack = () => {
+    navigate("/");
+  };
+
+  return (
+    <div className="login">
+      <img
+        src={arrowIcon}
+        alt="arrow-back"
+        onClick={handleBack}
+        className="login__arrow-back"
+      />
+      <Outlet context={auth} />
+    </div>
+  );
 }
 
-export {LoginPage}
+export { LoginPage };
